76ee12438fd9d5f2d9255e46795f2dbd0463e173
Naman Bajaj <namanbajaj08@gmail.com>
2020-09-04 14:03:08 +0530

    Azure Resource Group Creation through TF

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+// Resource Group Module outputs
+
+output "id" {
+    value = azurerm_resource_group.resource_group.id
+    description = "id of the resource group"
+
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+// Resource Group Module variables
+
+variable "name" {
+  type    = string
+
+}
+
+variable "location" {
+  type    = string
+
+}
